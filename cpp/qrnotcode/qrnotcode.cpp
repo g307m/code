7d5759c6a3908447705c7415dd8274e0eb29e691
glxl <grantlmul@gmail.com>
2019-03-06 12:00:59 -0500

    is qrcode

---
+++ cpp/qrnotcode/qrnotcode.cpp
@@ -0,0 +1,21 @@
+/*
+Creation:   1017,20190306
+Name(s):  Grant Mulholland
+Email(s): grantlmul@gmail.com
+*/
+#include <iostream>
+using namespace std;
+//Divides numbers with remainder included.
+int main(){
+  int divisor,dividend,quotient,remainder;
+  cout<<"Enter dividend:";
+  cin>>dividend;
+  cout<<"Enter divisor:";
+  cin>>divisor;
+  quotient=dividend/divisor;
+  remainder=dividend%divisor;
+  cout << "Quotient= " << quotient << endl;
+  cout << "Remainder= " << remainder;
+  cout << "\n";
+  return 0;
+}
