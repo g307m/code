ddff2a15a141aa2fa29c34c889662ad9a3deb163
glxl <grantlmul@gmail.com>
2019-03-06 18:54:21 -0500

    .gitignore updated. 17 Created and complete.

---
+++ cpp/17-asciihex/aschx.cpp
@@ -0,0 +1,17 @@
+/*
+Creation: 1847,20190306
+Name(s):  Grant Mulholland
+Email(s): grantlmul@gmail.com
+*/
+#include <iostream>
+using namespace std;
+/*
+Gets and returns the ASCII value of a character inputted by the user.
+*/
+int main(){
+    char c;
+    cout<<"Character for usage: ";
+    cin>>c;
+    cout<<"ASCII value of "<<c<<" is "<<int(c);
+    return 0;
+}
