715860b9a64597b139dfcd14e5da6bf2d5dd4b7d
glxl <grantlmul@gmail.com>
2019-03-06 13:12:43 -0500

    Create syssize,syssize/sys.cpp

---
+++ cpp/syssize/sys.cpp
@@ -0,0 +1,21 @@
+/*
+Creation: 1252,20190306
+Name(s):  Grant Mulholland
+Email(s): grantlmul@gmail.com
+*/
+#include <iostream>
+using namespace std;
+/*
+Prints the size of:
+    char
+    int
+    float
+    double
+*/
+int main(){
+    cout<<"Size of char:    "<<sizeof(char)<<" byte(s)"<<endl;
+    cout<<"Size of int:     "<<sizeof(int)<<" byte(s)"<<endl;
+    cout<<"Size of float:   "<<sizeof(float)<<" bytes(s)"<<endl;
+    cout<<"Size of double:  "<<sizeof(double)<<" byte(s)"<<endl;
+    return 0;
+}
